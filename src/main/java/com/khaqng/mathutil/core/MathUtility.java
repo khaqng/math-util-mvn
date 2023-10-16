6a80e65962d1e66c5421d5e9a63a5391e5f9d28a
khaqng <khanhquangnguyennqk@gmail.com>
2023-10-16 08:56:58 +0700

    update getf() to use recursion. see green4ever

--- src/main/java/com/khaqng/mathutil/core/MathUtility.java
+++ src/main/java/com/khaqng/mathutil/core/MathUtility.java
@@ -17,7 +17,9 @@ public class MathUtility {
     //0! = 1! quy ước 0! là 1
     //vì giai thừa tăng rất nhanh, 21! kiểu long chứa ko nổi (vượt 18 số 0)
     //do đó getFactorial() của chúng ta chỉ chơi với n = 0...20
-    public static long getFactorial(int n) {
+    
+    //ĐỆ QUY - RECURSION - GỌI LẠI CHÍNH MÌNH VỚI QUY MÔ NHỎ HƠN
+        public static long getFactorial(int n) {
 
         if (n < 0 || n > 20) 
             throw new IllegalArgumentException("Invalid n.n must be 0..20");
@@ -25,10 +27,35 @@ public class MathUtility {
         if (n == 0 || n == 1) 
             return 1; // nếu n đặc biệt, ta return sớm luôn
         
-        long result = 10;//sống sót đến lệnh này, sure n = 0..20
-
-        for (int i = 2; i <= n; i++)
-            result *= i;
-        return result;
+        return n * getFactorial(n-1);
+        
+        //tối ưu code, thay ruột của hàm để chạy ngon hơn, nhanh hơn, hoặc code viết đẹp hơn
+        //hoặc fix bug thì phải sửa code
+        //DÙ BẠN SỬA CODE VÌ LÍ DO GÌ, THÌ CODE PHẢI ĐƯỢC TEST LẠI 
+        //VÀ TEST ;LẠI ĐỂ ĐẢM BẢO RẰNG NÓ VẪN XANH NHƯ LẦN TRƯỚC
+        //NẾU TA VIẾT CODE ĐI KÈM TEST SCRIPT THÌ VIỆC TEST LẠI CÁI
+        //HÀM SẼ CỰC NHANH, KO CẦN DÙNG MẮT ĐỂ DÒ TỪNG DÒNG LỆNH ĐÚNG, SAI
+        //MÀ CHỈ NHÌN MÀU XANH LÀ ĐỦ - VÌ HÀM TRẢ VỀ NHƯ EXPECTED
+        //TEST LẠI NHỮNG GÌ ĐÃ TỪNG TEST -> REGRESSION TEST
+        //                                  KIỂM THỬ HỒI QUY
+        // KIỂM THỬ LẠI HÀM LÚC NÀY CỰC NHANH, VÌ GIT HUB ACTIONS LO GIÙM
+        //TA CHỈ CẦN MÀU XANH, HOẶC NHẬN EMAIL CHỬI VÌ CODE ĐỎ
+        
     }
+    
+    
+//    public static long getFactorial(int n) {
+//
+//        if (n < 0 || n > 20) 
+//            throw new IllegalArgumentException("Invalid n.n must be 0..20");
+//
+//        if (n == 0 || n == 1) 
+//            return 1; // nếu n đặc biệt, ta return sớm luôn
+//        
+//        long result = 1;//sống sót đến lệnh này, sure n = 0..20
+//
+//        for (int i = 2; i <= n; i++)
+//            result *= i;
+//        return result;
+//    }
 }
